fd578f5e341a89e9c7633103e52372ed88ef9d21
somaia96 <88092847+somaia96@users.noreply.github.com>
2023-01-24 13:37:30 +0300

    clock

--- js/main.js
+++ js/main.js
@@ -16,3 +16,34 @@ let colorfulDiv = document.querySelector(".section1 .container .part:nth-of-type
 setInterval(function(){
     colorfulDiv.style.cssText=`background-image:linear-gradient(-45deg,rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}) ${Math.floor(Math.random()*70)}%,rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}) 100%);`;
     },1000)
+
+    // clock section
+    
+    let mydate = new Date();
+    let hour = mydate.getHours();
+    let min = mydate.getMinutes();
+    let sec = mydate.getSeconds();
+    console.log(hour1.ariaValueNow);
+    let a=sec * 6 - 90;
+    let b=min * 6 - 90;
+    let c=hour * 30 - 90;
+    span3.style.cssText=`transform:rotate(${a}deg) translate(0%,-50%);`
+    span2.style.cssText=`transform:rotate(${b}deg) translate(0%,-50%);`
+    span1.style.cssText=`transform:rotate(${c}deg) translate(0%,-50%);`
+
+    setInterval(function(){
+        span3.style.cssText=`transform:rotate(${a+= 6}deg) translate(0%,-50%);`
+        if(a >=360){
+            a=0;
+        span2.style.cssText=`transform:rotate(${b+= 6}deg) translate(0%,-50%);`
+        if(b >= 360){
+            b=0;
+        span1.style.cssText=`transform:rotate(${c+= 30}deg) translate(0%,-50%);`
+        if(c >= 360){
+            c=0;
+        }
+    }
+    }
+    },1000)
+    
+    // 
